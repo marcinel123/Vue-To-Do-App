95ff67617bb4c6e41f659b8ebd216d0e0bd542f1
Marcin Eljasiewicz <m.eljasiewicz@gmail.com>
2022-10-25 07:19:59 +0200

    vue refs

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,19 @@
 <script setup>
+import { computed, ref } from 'vue';
+
+
+
+const todos = ref([])
+const name = ref([])
+
+const input_content = ref("")
+const input_category = ref(null)
+
+const todos_sorted = computed()
 
 </script>
+
+
 
 <template>
    Hello!
