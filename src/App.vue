3804fca6a66ec035151699fb1d17d80a6cb60853
Marcin Eljasiewicz <m.eljasiewicz@gmail.com>
2022-10-26 07:12:38 +0200

    added next logic and buttons

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,70 @@
 <script setup>
 import { computed, onMounted, ref, watch } from "vue";
 
-const todos = ref([]);
 const name = ref("");
+const todos = ref([]);
 
 const input_content = ref("");
 const input_category = ref(null);
 
-// const todos_sorted = computed();
+const todos_sorted = computed(() => {
+  return todos.value.sort((a, b) => b.createdAt - a.createdAt);
+});
+
+const addTodo = () => {
+  if (input_content.value.trim() === "" || input_category.value === null) {
+    return;
+  }
+
+  todos.value.push({
+    content: input_content.value,
+    category: input_category.value,
+    done: false,
+    createdAt: new Date().getTime(),
+  });
+
+  input_content.value = "";
+  input_category.value = null;
+};
 
 watch(name, (newVal) => {
-  localStorage.setItem("name", newVal)
-})
+  localStorage.setItem("name", newVal);
+});
 
-onMounted(()=>{
-  name.value = localStorage.getItem("name") || ""
-})
+watch(
+  todos,
+  (newVal) => {
+    localStorage.setItem("todos", JSON.stringify(newVal));
+  },
+  { deep: true }
+);
 
+onMounted(() => {
+  name.value = localStorage.getItem("name") || "";
+  todos.value = JSON.parse(localStorage.getItem("todos")) || [];
+});
 
+const removeTodo = (todo) =>
+  (todos.value = todos.value.filter((t) => t != todo));
 </script>
 
 <template>
   <main class="app">
     <section class="header">
       <h2 class="main-heading">
-        Hello Dear <input 
-        type="text" 
-        placeholder="your name"
-        v-model="name" />
+        Hello Dear <input type="text" placeholder="your name" v-model="name" />
       </h2>
     </section>
 
     <section class="create-todo">
-      <form id="new-todo-form">
+      <form @submit.prevent="addTodo" id="new-todo-form">
         <h4>Add your next todo!</h4>
         <input
           type="text"
           id="todo-content"
           placeholder="your next todo..."
           name="content"
+          v-model="input_content"
         />
 
         <h4>Select your category.</h4>
@@ -49,6 +75,7 @@ onMounted(()=>{
               name="category"
               id="category_first"
               value="work"
+              v-model="input_category"
             />
             <span class="checkbox work"></span>
             <p>Work</p>
@@ -59,18 +86,38 @@ onMounted(()=>{
               name="category"
               id="category_second"
               value="personal"
+              v-model="input_category"
             />
             <span class="checkbox personal"></span>
             <p>Personal</p>
           </label>
         </div>
-        <button type="submit">Add todo</button>
+        <button>Add todo</button>
       </form>
     </section>
 
     <section class="todo-list">
       <h3>TODO LIST!</h3>
-      <div class="list" id="todo-list"></div>
+      <div class="list" id="todo-list">
+        <div
+          v-for="todo in todos_sorted"
+          :key="todo.createdAt"
+          :class="`todo-item ${todo.done && 'done'}`"
+        >
+          <label>
+            <input type="checkbox" v-model="todo.done" />
+            <span :class="`checkbox ${todo.category}`"></span>
+          </label>
+
+          <div class="todo-content">
+            <input type="text" v-model="todo.content" />
+          </div>
+
+          <div class="buttons">
+            <button class="delete" @click="removeTodo(todo)">Delete</button>
+          </div>
+        </div>
+      </div>
     </section>
   </main>
 </template>
